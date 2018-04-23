6d4e3911f44ed8d30a6e7551c82aa74c378e12d6
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-04-23 12:14:05 -0700

    updated logo

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './footer.css';
 
 const styles = {
-  height: '150px',
+  height: '140px',
   width: '100%',
   backgroundColor: 'white',
   display: 'flex',
@@ -10,9 +10,9 @@ const styles = {
 
 export default props => (
   <footer style={styles} className="footer">
-    <div style={{minHeight: '20vh', display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end'}}>
+    <div style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end'}}>
       <img style={{ height: '100%', flex: 0, paddingLeft: '20px'}} src={ require('./logo.svg') } alt="premier logo"/>
-      <h1 style={{ flex: 1, marginBottom: '11px', textTransform: 'uppercase', color: 'rgb(120, 41, 28)', fontSize: '1.5em'}}>Premier Alliances, Inc.</h1>
+      <h1 style={{ flex: 1, marginBottom: '8px', textTransform: 'uppercase', color: 'rgb(120, 41, 28)', fontSize: '1.3em'}}>Premier Alliances, Inc.</h1>
     </div>
     <div>
       <p>Copyright © 2018 Premier Alliances, Inc. All rights reserved.</p>
