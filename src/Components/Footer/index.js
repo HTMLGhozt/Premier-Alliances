fb74e72a0a1a34e67b40624b1fe2ad358d8b73ff
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-04-25 09:37:55 -0700

    minor tweaks

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -11,11 +11,10 @@ const styles = {
 export default props => (
   <footer style={styles} className="footer">
     <div style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end'}}>
-      <img style={{ height: '90%', flex: 0, paddingLeft: '100px'}} src={ require('./logo.svg') } alt="premier logo"/>
+      <img style={{ height: '80%', flex: 0, paddingLeft: '100px', paddingBottom: '10px'}} src={ require('./logo.svg') } alt="premier logo"/>
       {/* <img style={{marginBottom: '8px'}} src={require('../../PA_typeset.svg')} /> */}
-      <h1 style={{ flex: 1, marginBottom: '8px', textTransform: 'uppercase', color: 'rgb(120, 41, 28)', fontSize: '1.15em'}}>Premier Alliances, Inc.</h1>
-    </div>
-    <div>
+      {/* <h1 style={{ flex: 1, marginBottom: '8px', textTransform: 'uppercase', color: 'rgb(120, 41, 28)', fontSize: '1.15em'}}>Premier Alliances, Inc.</h1> */}
+      <div style={{ paddingLeft: '15px'}}>
       <p>Copyright © 2018 Premier Alliances, Inc. All rights reserved.</p>
       <p>
         Office: 520.335.0373<br/>
@@ -23,6 +22,16 @@ export default props => (
         Sierra Vista, AZ 85635<br/>
         info@premieralliances.org<br/>
       </p>
+      </div>
+    </div>
+    <div>
+      {/* <p>Copyright © 2018 Premier Alliances, Inc. All rights reserved.</p>
+      <p>
+        Office: 520.335.0373<br/>
+        1993 Frontage Rd. | Ste. 104<br/>
+        Sierra Vista, AZ 85635<br/>
+        info@premieralliances.org<br/>
+      </p> */}
     </div>
   </footer>
 );
